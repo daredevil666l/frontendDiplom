576d22b2a4ade525667d5585abe4dc786bc6910c
Артем Гаврилов <artem.523artem@yandex.ru>
2025-11-25 18:07:29 +0500

    настройка под github pages

--- eslint.config.js
+++ eslint.config.js
@@ -2,37 +2,30 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-    ],
-    plugins: {
-      'react-refresh': reactRefresh,
-      'react-hooks': reactHooks,
-    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
       },
     },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
+      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
-      'semi': ['error', 'always', { 'omitLastInOneLineBlock': false }],
-      'quotes': ['error', 'single'],
-      'no-unused-vars': 'warn',
     },
   },
-])
+]
